20e8ea311f9c41e957622ff00d7353a356c9dd72
mubashir <mubashir2k17@gmail.com>
2018-10-06 13:07:49 +0500

    added some code

--- gameJam/ViewController.swift
+++ gameJam/ViewController.swift
@@ -10,14 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var characterCollectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        characterCollectionView.register(UINib(nibName: "CharaterCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "CharaterCollectionViewCell")
+        characterCollectionView.dataSource = self
+        characterCollectionView.delegate = self
+    }
+}
+
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 6
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CharaterCollectionViewCell", for: indexPath) as? CharaterCollectionViewCell {
+            return cell
+        }
+        return CharaterCollectionViewCell()
     }
 
 
