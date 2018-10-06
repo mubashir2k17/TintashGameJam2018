917e6a21d6954cc32d16c87b46e0c25f2e8ee5d6
khunshan <ahmad.khunshan@gmail.com>
2018-10-06 13:52:02 +0500

    added idle zoombie anim

---
+++ gameJam/Utility.swift
@@ -0,0 +1,22 @@
+//
+//  Utility.swift
+//  gameJam
+//
+//  Created by khunshan on 06/10/2018.
+//  Copyright © 2018 Tintash. All rights reserved.
+//
+
+import UIKit
+
+func animatedImage(asset: String, startIndex: Int, endIndex: Int) -> UIImage? {
+    
+    var arr = [UIImage]()
+    
+    for i in startIndex...endIndex {
+        let filename = "\(asset)\(i)"
+        arr.append(UIImage(named: filename)!)
+    }
+    
+    let image = UIImage.animatedImage(with: arr, duration: 1)
+    return image
+}
