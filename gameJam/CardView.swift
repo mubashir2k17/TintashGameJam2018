ca1e55058a21f6f67f18ac8240f4ad1e9ddb4553
Salman Akram <salman.akram@tintash.com>
2018-10-06 15:39:43 +0500

    Die card Animantion.

--- gameJam/CardView.swift
+++ gameJam/CardView.swift
@@ -13,7 +13,29 @@ class CardView: UIView {
     var position : (rowNum : Int, columnNum : Int) = (0,0)
     
     func die() {
-        
+        UIView.animate(withDuration: 0.1,
+                       delay: 0.2,
+                       options: [.repeat, .autoreverse],
+                       animations: {
+                        UIView.setAnimationRepeatCount(2)
+                        let transform = self.transform
+                        self.transform = self.transform.rotated(by: 0.07)
+                        self.transform = transform
+        },
+                       completion: { (true) in
+                        UIView.animate(withDuration: 0.3,
+                                       delay: 0.0,
+                                       options: [.curveEaseInOut],
+                                       animations: {
+                                        let width = self.frame.size.width
+                                        let heigth = self.frame.size.height
+                                        let x = self.frame.origin.x
+                                        let y = self.frame.origin.y
+                                        self.frame = CGRect(x: x+10, y: y+10, width: width-20, height: heigth-20)
+                                        self.alpha = 0.0
+                        },
+                                       completion: nil)
+        })
     }
 
     func moveLeft() {
