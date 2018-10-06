0878198490b3dd527cd76709372a0a16786198eb
khunshan <ahmad.khunshan@gmail.com>
2018-10-06 17:35:44 +0500

    more assets

--- gameJam/CharaterCollectionViewCell.swift
+++ gameJam/CharaterCollectionViewCell.swift
@@ -18,8 +18,9 @@ class CharaterCollectionViewCell: UICollectionViewCell {
 
         //imgView.image = animatedImage(asset: "zombieIdle", startIndex: 1, endIndex: 15)
         //imgView.image = animatedImage(asset: "robotIdle", startIndex: 0, endIndex: 8)
-        imgView.image = animatedImage(asset: "robotGoldenIdle", startIndex: 0, endIndex: 8)
-        
+        //imgView.image = animatedImage(asset: "robotGoldenIdle", startIndex: 0, endIndex: 8)
+        imgView.image = animatedImage(asset: "trollGreenIdle", startIndex: 0, endIndex: 9, duration: 1)
+
     }
 
 }
