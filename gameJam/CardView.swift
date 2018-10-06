dbc72bb4aaa10c8a0db483314bf7fce63d3c80a8
Abdur Rafay <abdul.rafay@tintash.com>
2018-10-06 13:49:12 +0500

    Card class added

---
+++ gameJam/CardView.swift
@@ -0,0 +1,33 @@
+//
+//  CardView.swift
+//  gameJam
+//
+//  Created by AbdurRafay on 06/10/2018.
+//  Copyright © 2018 Tintash. All rights reserved.
+//
+
+import UIKit
+
+class CardView: UIView {
+
+    func die() {
+        
+    }
+
+    func moveLeft() {
+        
+    }
+    
+    func moveRight() {
+        
+    }
+    
+    func moveUp() {
+        
+    }
+    
+    func moveDown() {
+        
+    }
+    
+}
