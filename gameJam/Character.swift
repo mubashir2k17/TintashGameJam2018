ccff6b6e00b4d7e207179b87657f143b246456ef
mubashir <mubashir2k17@gmail.com>
2018-10-07 00:45:17 +0500

    added knightIdle to backgorund

--- gameJam/Character.swift
+++ gameJam/Character.swift
@@ -14,7 +14,16 @@ class Character: CardView {
     var maxARmor  = 8
     var currentHealth = 10
     var currentArmor = 0
-    
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        super.cardBackgroundImageView.image = animatedImage(asset: "knightIdle", startIndex: 0, endIndex: 6)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
     func addHealth(healthValue : Int) {
         currentHealth = min(currentHealth + healthValue, maxHealth)
     }
